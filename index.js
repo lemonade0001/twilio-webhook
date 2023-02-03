af627400ae7c5109126df9d5b7f9cc19bd9b0ad8
lemonade0001 <easywhitetree@gmail.com>
2023-02-04 00:40:17 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { MessagingResponse } = require('twilio').twiml;
+
+const app = express();
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('Thank you for sending message!');
+
+  res.type('text/xml').send(twiml.toString());
+});
+
+app.post('/voice', (request, response) => {
+    // Use the Twilio Node.js SDK to build an XML response
+    const twiml = new VoiceResponse();
+    twiml.say({ voice: 'alice' }, 'hello world!');
+  
+    // Render the response as XML in reply to the webhook request
+    response.type('text/xml');
+    response.send(twiml.toString());
+  });
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000');
+});
